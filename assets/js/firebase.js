09059145ad60d898555652e919e90ccf7370feab
Alejandro <alejandrovalenciav9614@gmail.com>
2020-09-22 13:10:34 -0500

    upload All files

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDb5TvVVEV3y9Qx-2Nr7-73cAtCBnzN_KE",
+    authDomain: "chat-app-3a017.firebaseapp.com",
+    databaseURL: "https://chat-app-3a017.firebaseio.com",
+    projectId: "chat-app-3a017",
+    storageBucket: "chat-app-3a017.appspot.com",
+    messagingSenderId: "282636622094",
+    appId: "1:282636622094:web:54ba5d7033e02246cf9f5f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
